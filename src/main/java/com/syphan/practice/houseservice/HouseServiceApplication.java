e7794cfc4f6a852b620aac0b3e13cec1efa995d1
syphan <phantiensy@unimarmores.com>
2019-10-16 12:37:34 +0700

    refactor code and file pom

--- src/main/java/com/syphan/practice/houseservice/HouseServiceApplication.java
+++ src/main/java/com/syphan/practice/houseservice/HouseServiceApplication.java
@@ -2,8 +2,14 @@ package com.syphan.practice.houseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
+@EnableAsync
 public class HouseServiceApplication {
 
     public static void main(String[] args) {
